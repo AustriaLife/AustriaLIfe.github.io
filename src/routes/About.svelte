557f5d76816a05c7c1a1552d8169cbed23694993
Chris Oesterreich <chrisoesterreichprog@gmail.com>
2023-11-07 14:06:58 +0100

    using svelte kit

--- src/routes/About.svelte
+++
@@ -1,4 +0,0 @@
-<main>
-    <h1>Hello </h1>
-	<p>ft7zugih</p>
-</main>
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home.svelte';
-import About from './routes/About.svelte';
-import Contact from './Contact.svelte';
-
-export const routes = {
-  '/': Home,
-  '/about': About,
-  '/contact': Contact,
-};
